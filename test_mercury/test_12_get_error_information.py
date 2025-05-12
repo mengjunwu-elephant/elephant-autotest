ba691cde381ee4cb06352775c1584d365664d1bc
mengjunwu <mengjun.wu@elephantrobotics.com>
2025-05-12 15:11:16 +0800

    Update test_mercury

---
+++ test_mercury/test_12_get_error_information.py
@@ -0,0 +1,122 @@
+import unittest
+from ddt import ddt, data
+
+from common1.test_data_handler import get_test_data_from_excel
+from common1 import logger
+from settings import TestMercury
+
+# 从Excel中提取数据
+cases = get_test_data_from_excel(TestMercury.TEST_DATA_FILE, "get_error_information")
+
+
+@ddt
+class TestGetErrorInformation(unittest.TestCase):
+    # 实例化日志模块
+    logger = logger
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        水星系列初始化先左臂上电，后右臂上电
+        """
+        cls.device = TestMercury()
+        cls.device.ml.power_on()
+        cls.device.mr.power_on()
+        cls.logger.info("初始化完成，接口测试开始")
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        下电顺序为先右臂下电，后左臂下电
+        :return:
+        """
+        cls.device.mr.power_off()
+        cls.device.ml.power_off()
+        cls.logger.info("环境清理完成，接口测试结束")
+
+    def tearDown(self):
+        self.device.ml.clear_error_information()
+        self.device.mr.clear_error_information()
+        self.device.go_zero()
+
+    @data(*[case for case in cases if case.get("test_type") == "normal"])
+    def test_singular_point_error(self, case):
+        self.logger.info('》》》》》用例【{}】开始测试《《《《《'.format(case['title']))
+        # 调试信息
+        self.logger.debug('test_api:{}'.format(case['api']))
+        self.logger.debug('test_parameter:{}'.format(case['parameter']))
+        # 使机械臂位于奇异点，并发送坐标
+        self.device.ml.send_angle(6,0,self.device.speed)
+        self.device.mr.send_angle(6,0,self.device.speed)
+        self.device.ml.send_coord(3,300,self.device.speed)
+        self.device.mr.send_coord(3, 300, self.device.speed)
+        input(print("请观察机械臂末端是否变蓝，点击回车继续测试"))
+        # 左臂请求发送
+        l_response = self.device.ml.get_error_information()
+
+        # 右臂请求发送
+        r_response = self.device.mr.get_error_information()
+        try:
+            # 请求结果类型断言
+            if type(l_response) == int:
+                self.logger.debug('左臂请求类型断言成功')
+            else:
+                self.logger.debug('左臂请求类型断言失败，实际类型为{}'.format(type(l_response)))
+            if type(r_response) == int:
+                self.logger.debug('右臂请求类型断言成功')
+            else:
+                self.logger.debug('右臂请求类型断言失败，实际类型为{}'.format(type(r_response)))
+            # 请求结果断言
+            self.assertEqual(case['r_expect_data'], r_response)
+            self.assertEqual(case['l_expect_data'], l_response)
+        except AssertionError as e:
+            self.logger.exception('请求结果断言失败')
+            self.logger.debug('左臂期望数据：{}'.format(case['l_expect_data']))
+            self.logger.debug('右臂期望数据：{}'.format(case['r_expect_data']))
+            self.logger.debug('左臂实际结果：{}'.format(l_response))
+            self.logger.debug('右臂实际结果：{}'.format(r_response))
+            raise e
+        else:
+            self.logger.info('请求结果断言成功,用例【{}】测试成功'.format(case['title']))
+        finally:
+            self.logger.info('》》》》》用例【{}】测试完成《《《《《'.format(case['title']))
+
+    @data(*[case for case in cases if case.get("test_type") == "exception"])
+    def test_get_error_information(self,case):
+        self.logger.info('》》》》》用例【{}】开始测试《《《《《'.format(case['title']))
+        # 调试信息
+        self.logger.debug('test_api:{}'.format(case['api']))
+        self.logger.debug('test_parameter:{}'.format(case['parameter']))
+
+        # 左臂请求发送
+        l_response = self.device.ml.get_error_information()
+
+        # 右臂请求发送
+        r_response = self.device.mr.get_error_information()
+        try:
+            # 请求结果类型断言
+            if type(l_response) == int:
+                self.logger.debug('左臂请求类型断言成功')
+            else:
+                self.logger.debug('左臂请求类型断言失败，实际类型为{}'.format(type(l_response)))
+            if type(r_response) == int:
+                self.logger.debug('右臂请求类型断言成功')
+            else:
+                self.logger.debug('右臂请求类型断言失败，实际类型为{}'.format(type(r_response)))
+            # 请求结果断言
+
+                self.assertEqual(case['r_expect_data'], r_response)
+                self.assertEqual(case['l_expect_data'], l_response)
+        except AssertionError as e:
+            self.logger.exception('请求结果断言失败')
+            self.logger.debug('左臂期望数据：{}'.format(case['l_expect_data']))
+            self.logger.debug('右臂期望数据：{}'.format(case['r_expect_data']))
+            self.logger.debug('左臂实际结果：{}'.format(l_response))
+            self.logger.debug('右臂实际结果：{}'.format(r_response))
+            raise e
+        else:
+            self.logger.info('请求结果断言成功,用例【{}】测试成功'.format(case['title']))
+        finally:
+            self.logger.info('》》》》》用例【{}】测试完成《《《《《'.format(case['title']))
+
+
